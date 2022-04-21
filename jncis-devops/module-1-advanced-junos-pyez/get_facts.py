c0cb6f59b8d9b3c7a0f84b27200381733681094a
Stamatis Maritsas <stamatis.maritsas@gmail.com>
2022-04-21 13:33:31 +0100

    change passwd in get_facts.py

--- jncis-devops/module-1-advanced-junos-pyez/get_facts.py
+++ jncis-devops/module-1-advanced-junos-pyez/get_facts.py
@@ -3,8 +3,8 @@
 from jnpr.junos import Device
 
 if __name__ == '__main__':
-    dev = Device(host='172.25.11.1', user='brook')
+    dev = Device(host='172.25.11.1', user='brook', passwd='onepiece123')
     dev.open()
-    print(dev.facts())
-    dev.close()
+    print(dev.facts)
+    dev.close
 
